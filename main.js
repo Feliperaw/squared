e1b023e6fd2e6bfaa1bb30166e37c773f06abc33
Daniel <danideckin@gmail.com>
2023-01-24 17:14:09 -0500

    Agregado previsualizacion del producto

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burgerBtn = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const carBtn = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector ('.product-detail');
+const shoppingCartContainer = document.querySelector ('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 
@@ -13,7 +13,7 @@ burgerBtn.addEventListener('click', toggleMobileMenu);
 carBtn.addEventListener('click', toggleProductDetail);
 
 function toggleDesktopMenu(){
-    const isAsideClosed = aside.classList.contains('inactive')
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
 
     if (!isAsideClosed) {
         aside.classList.add('inactive');
@@ -22,10 +22,10 @@ function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains('inactive')
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -37,7 +37,7 @@ function toggleProductDetail() {
         mobileMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 } 
 
 const productList = [];
